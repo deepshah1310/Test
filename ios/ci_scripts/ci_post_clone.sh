83d8116896f0f914e7886d9bbfb8bee2f12cb1c6
Deep <deeps@meditab.com>
2024-07-22 11:43:12 +0530

    -- Made changes in the gitignore.

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -9,11 +9,8 @@
 # Fail this script if any subcommand fails.
 set -e
 
-# The default execution directory of this script is the ci_scripts directory.
-#cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
-
 # Install Flutter using git.
-git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable "$HOME"/flutter
 export PATH="$PATH:$HOME/flutter/bin"
 
 # Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
